bff5e9629e2942791e50f66a817840fd3109ef18
Alex Stine <ads48@duke.edu>
2018-03-20 13:01:32 -0400

    Attribution to Arkady

--- main_task_180320.m
+++ main_task_180320.m
@@ -1,3 +1,6 @@
+% This bulk of this task code was written by Arkady Konovalov, PhD (University of Zurich) and generously shared on request.
+% I have altered the code for my purposes
+
 function main_task_180320(trials, practice,track)
 
     track = 0;
